47049f556bae37aef1bf44696c6e98947c261e59
dcontreras1204 <017101011k@uandina.edu.pe>
2017-06-24 13:34:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dexter/proyappdestinos/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dexter.proyappdestinos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
